2783d62cf5b98980740189f59483898804af330d
YarPodl <yar.podlesnov@yandex.ru>
2018-02-14 17:10:03 +0300

    Текущая

--- lab2/lab2/Form1.cs
+++ lab2/lab2/Form1.cs
@@ -1,20 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace lab2
 {
-    public partial class Form1 : Form
+    public partial class Form : System.Windows.Forms.Form
     {
-        public Form1()
+        //List<APS> APSs = new List<APS>();
+        public Form()
         {
             InitializeComponent();
+        }
+
+        private void Form_Load(object sender, EventArgs e)
+        {
+            //APSs.Add(new APS("Наша станция", 12345, "ул. Рождественского, 51", 1100, 35.50, "Стандартный", 900));
+            listBox1.Items.Add(new APS("Наша станция", 12345, "ул. Рождественского, 51", 1100, 35.50, "Стандартный", 900));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            InputBox.InputBox inputBox = new InputBox.InputBox();
+            String s = inputBox.getString();
+            if (s != null)
+            {
+                listBox1.Items.Add(new APS(s));
+            }
         }
     }
 }
